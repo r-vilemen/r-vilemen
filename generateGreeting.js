67798f2619b0bdfb6168c65345e805fcf935bc48
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-10 15:37:49 -0300

    Update greeting in README.md and generateGreeting.js

--- generateGreeting.js
+++ generateGreeting.js
@@ -8,10 +8,10 @@ const getCurrentGreeting = () => {
 
   const hour = parseInt(brHour, 10);
 
-  if (hour >= 6 && hour < 12) return "Bom dia";
-  if (hour >= 12 && hour < 18) return "Boa tarde";
-  if (hour >= 18 && hour < 23) return "Boa noite";
-  return "Boa madrugada";
+  if (hour >= 6 && hour < 12) return "Bom dia! ";
+  if (hour >= 12 && hour < 18) return "Boa tarde! ";
+  if (hour >= 18 && hour < 24) return "Boa noite! ";
+  return "Boa madrugada! ";
 };
 
 const fs = require("fs");
