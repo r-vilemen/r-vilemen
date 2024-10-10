c181333f20f4e5fdd340d277bae228df317458f3
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-10 15:33:07 -0300

    Update generateGreeting.js to use local time for greetings

--- generateGreeting.js
+++ generateGreeting.js
@@ -1,10 +1,17 @@
 const getCurrentGreeting = () => {
-  const hour = new Date().getHours();
+  const options = {
+    timeZone: "America/Sao_Paulo",
+    hour: "numeric",
+    hour12: false,
+  };
+  const brHour = new Intl.DateTimeFormat("pt-BR", options).format(new Date());
 
-  if (hour >= 6 && hour < 12) return "Bom dia!";
-  if (hour >= 12 && hour < 18) return "Boa tarde!";
-  if (hour >= 18 && hour < 23) return "Boa noite!";
-  return "Boa madrugada!";
+  const hour = parseInt(brHour, 10);
+
+  if (hour >= 6 && hour < 12) return "Bom dia";
+  if (hour >= 12 && hour < 18) return "Boa tarde";
+  if (hour >= 18 && hour < 23) return "Boa noite";
+  return "Boa madrugada";
 };
 
 const fs = require("fs");
