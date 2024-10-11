8689fedd0401aa337d6b1843cb2b966a0cf5bb45
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-11 07:29:41 -0300

    Refactor greeting message to use correct Portuguese translation

--- generateGreeting.js
+++ generateGreeting.js
@@ -9,7 +9,7 @@ const getCurrentGreeting = () => {
   const brHour = new Intl.DateTimeFormat("pt-BR", options).format(new Date());
   const hour = parseInt(brHour, 10);
 
-  if (hour >= 6 && hour < 12) return "Bom 3";
+  if (hour >= 6 && hour < 12) return "Bom dia";
   if (hour >= 12 && hour < 18) return "Boa tarde";
   if (hour >= 18 && hour < 24) return "Boa noite";
   return "Boa madrugada";
