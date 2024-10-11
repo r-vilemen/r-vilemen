2a497a0d99d2cf26ef74315f9c95268ea3f9a95a
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-11 05:35:23 -0300

    fix: Update greeting generation logic and remove unnecessary file

--- generateGreeting.js
+++ generateGreeting.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+
+// Função para obter a saudação com base no horário de Brasília
 const getCurrentGreeting = () => {
   const options = {
     timeZone: "America/Sao_Paulo",
@@ -5,14 +8,26 @@ const getCurrentGreeting = () => {
     hour12: false,
   };
   const brHour = new Intl.DateTimeFormat("pt-BR", options).format(new Date());
-
   const hour = parseInt(brHour, 10);
 
-  if (hour >= 6 && hour < 12) return "Bom dia! ";
-  if (hour >= 12 && hour < 18) return "Boa tarde! ";
-  if (hour >= 18 && hour < 24) return "Boa noite! ";
-  return "Boa madrugada! ";
+  if (hour >= 6 && hour < 12) return "Bom dia!";
+  if (hour >= 12 && hour < 18) return "Boa tarde!";
+  if (hour >= 18 && hour < 23) return "Boa noite!";
+  return "Boa madrugada!";
 };
 
-const fs = require("fs");
-fs.writeFileSync("greeting.txt", getCurrentGreeting());
+// Lê o conteúdo atual do README.md
+const readmePath = "README.md";
+const readmeContent = fs.readFileSync(readmePath, "utf-8");
+
+// Gera a saudação
+const greeting = getCurrentGreeting();
+
+// Substitui o conteúdo entre os marcadores de saudação
+const updatedReadmeContent = readmeContent.replace(
+  /<!-- start-greeting -->.*<!-- end-greeting -->/,
+  `<!-- start-greeting --> ${greeting} <!-- end-greeting -->`
+);
+
+// Grava o conteúdo atualizado no README.md
+fs.writeFileSync(readmePath, updatedReadmeContent);
