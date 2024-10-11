b486964aa554724097f19ac83ebe9293564c17cb
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-11 05:40:23 -0300

    fix: remove !

--- generateGreeting.js
+++ generateGreeting.js
@@ -10,10 +10,10 @@ const getCurrentGreeting = () => {
   const brHour = new Intl.DateTimeFormat("pt-BR", options).format(new Date());
   const hour = parseInt(brHour, 10);
 
-  if (hour >= 6 && hour < 12) return "Bom dia!";
-  if (hour >= 12 && hour < 18) return "Boa tarde!";
-  if (hour >= 18 && hour < 23) return "Boa noite!";
-  return "Boa madrugada!";
+  if (hour >= 6 && hour < 12) return "Bom dia";
+  if (hour >= 12 && hour < 18) return "Boa tarde";
+  if (hour >= 18 && hour < 23) return "Boa noite";
+  return "Boa madrugada";
 };
 
 // Lê o conteúdo atual do README.md
