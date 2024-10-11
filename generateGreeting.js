25cfda4b033a61fa06f0bed0bb2481e5847f621f
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-11 06:22:40 -0300

    update: hour fixed

--- generateGreeting.js
+++ generateGreeting.js
@@ -12,7 +12,7 @@ const getCurrentGreeting = () => {
 
   if (hour >= 6 && hour < 12) return "Bom dia";
   if (hour >= 12 && hour < 18) return "Boa tarde";
-  if (hour >= 18 && hour < 23) return "Boa noite";
+  if (hour >= 18 && hour < 24) return "Boa noite";
   return "Boa madrugada";
 };
 
